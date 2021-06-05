a9cab27c22502987c125a1783c2333b785d501f7
zayacsvetlana1 <79018083+zayacsvetlana1@users.noreply.github.com>
2021-06-05 15:07:22 +0300

    Add files via upload

---
+++ Food/js/script.js
@@ -0,0 +1,30 @@
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import {openModal} from './modules/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+
+	calc();
+	cards();
+	forms('form', modalTimerId);
+	modal('[data-modal]', '.modal', modalTimerId);
+	slider({container: '.offer__slider', 
+		slide: '.offer__slide', 
+		nextArrow: '.offer__slider-next', 
+		prevArrow: '.offer__slider-prev', 
+		totalCounter: '#total', 
+		currentCounter: '#current', 
+		wrapper: '.offer__slider-wrapper', 
+		field: '.offer__slider-inner'
+	});
+	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+	timer('.timer', '2021-06-31');
+	
+});
